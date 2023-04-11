b3e9d16e39cb918a4dd41a4c635190163fc905d6
Alexey Efimov <18059014+LexDeBash@users.noreply.github.com>
2023-04-11 16:48:00 +0300

    Initial Commit

---
+++ MyFavoriteMovies/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyFavoriteMovies
+//
+//  Created by Alexey Efimov on 11.04.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    private let apiKey = "YOUR_API_KEY_HERE"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
